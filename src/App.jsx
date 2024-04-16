872313106baf6d366975dce134a99765761236c4
Phillip  Friedel <s0557917@htw-berlin.de>
2024-04-16 16:26:38 +0200

    Added tailwindcss and inter font

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@
 function App() {
   return (
     <>
-      <h1>Phillip Friedel</h1>
+      <h1 className="font-inter font-bold">PHILLIP FRIEDEL</h1>
+      <h1 className="font-inter font-bold">SOFTWARE DEVELOPER</h1>
       <p>Under construction...</p>
     </>
   )

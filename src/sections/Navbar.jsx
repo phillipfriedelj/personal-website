b341e3fafdc584f775f032bb6e48f095a57bff8f
Phillip  Friedel <s0557917@htw-berlin.de>
2024-04-17 13:27:05 +0200

    Updated Navbar

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -4,13 +4,13 @@ import '../styles/animations.css';
 
 export default function Navbar() {
 	return (
-		<nav className="w-full flex py-5 px-[50px]">
+		<nav className="w-full flex py-5 px-5 md:px-[50px] h-20">
 			<div className="flex-1 items-center flex">
 				<img src={userLogo} className="h-5"/>
 			</div>
-			<div className="flex space-x-[50px] items-center">
-				<a href='#' className="font-inter font-light cursor-pointer hover-underline-animation">Projects</a>
-				<a href='#' className="font-inter font-light cursor-pointer hover-underline-animation">About</a>
+			<div className="flex space-x-2 md:space-x-[50px] items-center">
+				<a href='#' className="font-inter text-xs md:text-base font-light cursor-pointer hover-underline-animation">Projects</a>
+				<a href='#' className="font-inter text-xs md:text-base font-light cursor-pointer hover-underline-animation">About</a>
 				<EmailButton text="hello@phillipfriedel.com" />
 			</div>
 		</nav>

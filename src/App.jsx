ce200c216eb23bb34f4bb894062de5207843d7b5
Phillip  Friedel <s0557917@htw-berlin.de>
2024-04-17 23:28:33 +0200

    Modified import due to failure during build

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Banner from "./sections/Banner";
 import KeyFacts from "./sections/KeyFacts";
 import Footer from "./sections/Footer";
 import Divider from "./components/dividers/Divider";
-import DividerDecoration from "./components/dividers/DividerDecoration";
+import DividerDecoration from "./components/dividers/DividerDecoration"
 
 function App() {
 	return (

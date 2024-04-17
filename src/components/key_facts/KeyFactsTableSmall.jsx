3fd5480b0cc7c9ec6c5a5b0894c035751253e8c9
Phillip  Friedel <s0557917@htw-berlin.de>
2024-04-17 19:39:43 +0200

    Modified key facts small layout

--- src/components/key_facts/KeyFactsTableSmall.jsx
+++ src/components/key_facts/KeyFactsTableSmall.jsx
@@ -3,24 +3,6 @@ export default function KeyFactsTableSmall() {
         <div className="md:hidden flex flex-col md:flex-row md:space-x-[10px] w-full md:w-[947px] space-y-1 flex-grow">
             <div className="flex flex-col space-y-1 md:space-y-[10px] flex-1">
                 <div className="flex w-full space-x-[10px] md:h-[184px] flex-1">
-                    <div className="w-full md:w-[250px] rounded-[10px] bg-mint-light border border-black p-2">
-                        <p className="font-inter font-semibold text-medium">LANGUAGES</p>
-                        <ul className="list-disc list-inside pl-3">
-                            <li className="list-item text-xs"><span className="relative -left-2">German - <span className="font-inter italic font-thin">Native</span></span></li>
-                            <li className="list-item text-xs"><span className="relative -left-2">Spanish - <span className="font-inter italic font-thin">Native</span></span></li>
-                            <li className="list-item text-xs"><span className="relative -left-2">English - <span className="font-inter italic font-thin">Fluent</span></span></li>
-                        </ul>
-                    </div>
-                    <div className="w-full md:w-[250px] rounded-[10px] bg-mint-light border border-black p-2">
-                        <p className="font-inter font-semibold text-medium">CERTIFICATIONS</p>
-                        <ul className="list-disc list-inside pl-3">
-                            <li className="list-item text-xs"><span className="relative -left-2">Scrum Master 1</span></li>
-                            <li className="list-item text-xs"><span className="relative -left-2">Product Owner 1</span></li>
-                            <li className="list-item text-xs"><span className="relative -left-2">Telekom Cloud Architect</span></li>
-                        </ul>
-                    </div>
-                </div>
-                {/* <div className="flex w-full space-x-[10px] flex-1"> */}
                     <div className="w-full md:w-[515px] md:h-[184px] rounded-[10px] bg-mint-dark border border-black p-2">
                         <p className="font-inter font-semibold text-medium">SKILLS</p>
                         <div className="flex flex-grow space-x-5">
@@ -39,6 +21,24 @@ export default function KeyFactsTableSmall() {
                             </ul>
                         </div>
                     </div>
+                    <div className="w-full md:w-[250px] rounded-[10px] bg-mint-light border border-black p-2 flex-1">
+                        <p className="font-inter font-semibold text-medium">LANGUAGES</p>
+                        <ul className="list-disc list-inside pl-3">
+                            <li className="list-item text-xs"><span className="relative -left-2">German - <span className="font-inter italic font-thin">Native</span></span></li>
+                            <li className="list-item text-xs"><span className="relative -left-2">Spanish - <span className="font-inter italic font-thin">Native</span></span></li>
+                            <li className="list-item text-xs"><span className="relative -left-2">English - <span className="font-inter italic font-thin">Fluent</span></span></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="flex w-full space-x-[10px] flex-1">
+                    <div className="w-full md:w-[250px] rounded-[10px] bg-mint-light border border-black p-2 flex-1">
+                            <p className="font-inter font-semibold text-medium">CERTIFICATIONS</p>
+                            <ul className="list-disc list-inside pl-3">
+                                <li className="list-item text-xs"><span className="relative -left-2">Scrum Master 1</span></li>
+                                <li className="list-item text-xs"><span className="relative -left-2">Product Owner 1</span></li>
+                                <li className="list-item text-xs"><span className="relative -left-2">Telekom Cloud Architect</span></li>
+                            </ul>
+                    </div>
                     <div className="w-full md:w-[515px] md:h-[140px] rounded-[10px] bg-mint-light border border-black p-2">
                         <p className="font-inter font-semibold text-medium">EDUCATION</p>
                         <div className="flex flex-col pl-2">
@@ -52,7 +52,7 @@ export default function KeyFactsTableSmall() {
                             </div>
                         </div>
                     </div>
-                {/* </div> */}
+                </div>
             </div>
             <div className="flex-1 w-full md:w-[510px] md:h-[588px] rounded-[10px] bg-mint-light border border-black p-2">
                 <p className="font-inter font-semibold text-medium">CAREER</p>
